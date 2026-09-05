d127d33a531b833157774c2bcf07c8b25fc19208
arseniy <160741254+arseniyGoryagin@users.noreply.github.com>
2026-09-05 15:07:20 +0300

    add server

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Listening on port 6379")
+
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+
+		conn.Write([]byte("+OK\r\n"))
+	}
+
 }
